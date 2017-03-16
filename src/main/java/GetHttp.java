1724f27bcf60f501a58f5c8d4129e3aa620b8ceb
carlos andres sanchez venegas <carlos.sanchez-v@mail.escuelaing.edu.co>
2017-03-16 17:33:32 -0500

    hola

---
+++ src/main/java/GetHttp.java
@@ -0,0 +1,56 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author carlo
+ */
+
+    
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
+ *
+ * @author estudiante
+ */
+public class GetHttp implements Runnable{
+    Socket clientSocket;
+    
+    public GetHttp(Socket clientSocket) {
+        this.clientSocket=clientSocket;
+    }
+            
+    @Override
+    public void run() {
+        try {
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+                String raiz = "/home/estudiante/Downloads/AREM/AREM/raiz";
+                String recurso = in.readLine().trim().split(" ")[1];
+                System.out.println("Llamo al recurso: "+recurso);
+                byte[] outputLine = Files.readAllBytes(Paths.get(raiz+recurso));
+                clientSocket.getOutputStream().write(outputLine);
+            }
+            clientSocket.close();
+        } catch (IOException ex) {
+            System.err.println("Error cargando la pagina");
+        }
+    }
+}
+
+
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,68 +1,40 @@
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import spark.ModelAndView;
-import spark.template.freemarker.FreeMarkerEngine;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.net.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-import static javax.measure.unit.SI.KILOGRAM;
-import javax.measure.quantity.Mass;
-import org.jscience.physics.model.RelativisticModel;
-import org.jscience.physics.amount.Amount;
-
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
-import static spark.Spark.*;
-
+/**
+ *
+ * @author 2105534
+ */
 public class Main {
-
-  public static void main(String[] args) {
-
-    port(Integer.valueOf(System.getenv("PORT")));
-    staticFileLocation("/public");
-
-    get("/hello", (req, res) -> {
-        RelativisticModel.select();
-        Amount<Mass> m = Amount.valueOf("12 GeV").to(KILOGRAM);
-        return "E=mc^2: 12 GeV = " + m.toString();
-     });
-
-    get("/", (request, response) -> {
-        Map<String, Object> attributes = new HashMap<>();
-        attributes.put("message", "Hello World!");
-
-        return new ModelAndView(attributes, "index.ftl");
-    }, new FreeMarkerEngine());
-
-    HikariConfig config = new  HikariConfig();
-    config.setJdbcUrl(System.getenv("JDBC_DATABASE_URL"));
-    final HikariDataSource dataSource = (config.getJdbcUrl() != null) ?
-      new HikariDataSource(config) : new HikariDataSource();
-
-    get("/db", (req, res) -> {
-      Map<String, Object> attributes = new HashMap<>();
-      try(Connection connection = dataSource.getConnection()) {
-        Statement stmt = connection.createStatement();
-        stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-        ArrayList<String> output = new ArrayList<String>();
-        while (rs.next()) {
-          output.add( "Read from DB: " + rs.getTimestamp("tick"));
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = null;
+        //Integer port = new Integer(System.getenv("PORT"));
+        try {
+            //System.out.println(port);
+            serverSocket = new ServerSocket(35000);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: ");//+port);
+            System.exit(1);
         }
-
-        attributes.put("results", output);
-        return new ModelAndView(attributes, "db.ftl");
-      } catch (Exception e) {
-        attributes.put("message", "There was an error: " + e);
-        return new ModelAndView(attributes, "error.ftl");
-      }
-    }, new FreeMarkerEngine());
-
-  }
-
+        
+        // Concurrente
+        Socket clientSocket = null;
+        ExecutorService ex = Executors.newFixedThreadPool(10);
+        GetHttp tarea;    
+        while(true){
+            clientSocket = serverSocket.accept();
+            tarea = new GetHttp(clientSocket);
+            ex.execute(tarea);
+        }
+    }
 }
+
